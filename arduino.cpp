69fafd9cdf94a44ea5b3b820be906f294ebbd22f
cagla0117 <86843074+cagla0117@users.noreply.github.com>
2022-05-02 16:41:37 +0300

    Create arduino.cpp

---
+++ arduino.cpp
@@ -0,0 +1,39 @@
+// C++ code
+//
+void setup()
+{
+  
+  pinMode (1,OUTPUT);
+  pinMode (2,OUTPUT);
+  pinMode (3,OUTPUT);
+  pinMode (4,OUTPUT);
+  pinMode (5,OUTPUT);
+}
+
+void loop()
+{
+     digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(1, LOW);
+  delay(1000);
+ 
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+ 
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+  delay(1000);
+ 
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(1000);
+ 
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(5, LOW);
+  delay(1000);
+}
